c197f5cf9d32b451a3e7a7f7c6abfcd22000b13f
Jisoo-Lee93 <ljsoo0925@gmail.com>
2019-01-28 16:35:23 +0900

    unknown

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -2,4 +2,4 @@ export { default as Home } from './Home';
 export { default as Test } from './Test';
 export { default as Queue } from './Queue';
 export { default as Stack } from './Stack';
-export { default as PriorityQueue } from './Queue';
+export { default as PriorityQueue } from './PriorityQueue';
