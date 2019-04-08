c179fc98c5e832654090dffe2c5dd466b92d4c69
zerone2 <yicho93@naver.com>
2019-04-09 01:15:50 +0900

    Add build command to config.yml
    - build/ (trailing slash required**) -> change dir name to stl

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 ## file transfer
-rsync -avP ./web pi@$server_uri:/home/pi/stl -e "ssh -o StrictHostKeyChecking=no"
-
-## server restart
-ssh pi@$server_uri -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no <<'ENDSSH'
-npm run build
-npm start
-ENDSSH
+rsync -avP web/build/ pi@$server_uri:/home/pi/stl -e "ssh -o StrictHostKeyChecking=no"
